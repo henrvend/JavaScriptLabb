8cefb19f2f8612f690f21333a14209e8ac48f650
Henrik <henrvend@student.kau.se>
2023-03-22 11:52:28 +0100

    finished labb 3 with comments and all

--- js/JSFunctions.js
+++ js/JSFunctions.js
@@ -42,7 +42,75 @@ window.addEventListener('load', function() {
     //Hämtar element från ID i index.html och adderar en lysnare som vid klick kallar på metoden validateForm. 
     let newGame = document.getElementById("newGame");
     newGame.addEventListener("click", validateForm);
+
+    
 });
+
+
+function executeMove(event) {
+
+    //Tilldelar variabler
+    let jumbotronH1 = document.querySelector('.jumbotron h1');
+    let table = document.querySelector("table");
+    let et = event.target;
+    let form = document.querySelector("form");
+    let gameArea = document.querySelector("#game-area")
+    
+    //Kontrollerar ifall elemetetet är en TD.
+    if(et.tagName === "TD"){
+        //Skriver över TD med värdet för data-ID för att möjliggöra väl av TD i senare skede.
+        et = et.getAttribute("data-id");
+        
+        //Om fältet i gamefield är tom går den in i if-satsen
+        if(oGameData.gameField[et]===""){
+            
+            //Om nuvarande spelare är "X" går den in och sätter alla värden efter det.
+            if(oGameData.currentPlayer==oGameData.playerOne) {
+                jumbotronH1.textContent = "Aktuell spelare är "+ oGameData.nickNamePlayerTwo + "("+oGameData.playerTwo +")" ;;
+                event.target.style.backgroundColor = oGameData.colorPlayerOne;
+                event.target.textContent = oGameData.playerOne;
+                oGameData.gameField[et] = "X";
+                oGameData.currentPlayer=oGameData.playerTwo;
+            }
+            //Annars sätter den värdet efter "O"
+            else {
+                jumbotronH1.textContent = "Aktuell spelare är "+ oGameData.nickNamePlayerOne + "("+oGameData.playerOne +")";
+                event.target.style.backgroundColor = oGameData.colorPlayerTwo;
+                event.target.textContent = oGameData.playerTwo;
+                oGameData.gameField[et] = "O";
+                oGameData.currentPlayer=oGameData.playerOne;
+            }
+        }
+        
+        //Tilldelar variabeln 0, 1, 2, eller 3 beroende på spelets status.
+        let result = oGameData.checkForGameOver();
+        
+        //Kollar ifall result skiljer sig med 0, vilket innebär att det är oavgjort eller att någon av spelarna vunnit.
+        if(result != 0) {
+            if(result == 3){
+                jumbotronH1.textContent = "Oavgjort";
+            }
+            else if(result == 1) {
+                jumbotronH1.textContent ="Vinnare är "+oGameData.nickNamePlayerOne+"! Spela igen?";
+            }
+            else {
+                jumbotronH1.textContent = "Vinnare är " +oGameData.nickNamePlayerTwo+"! Spela igen?";
+            }
+            
+            //Tar bort lyssnare för tabellen.
+            table.removeEventListener("click", executeMove);
+            
+            //Tar bort klassen d-none from form, vilket resulterar att formuläret visas igen.
+            form.classList.remove("d-none");
+            
+            //Lägger till klassen d-none till gameArea vilket döljer spelplanen.
+            gameArea.classList.add("d-none");
+
+            //Initierar initGlobalObject igen
+            oGameData.initGlobalObject();
+        }  
+    }
+}
 
 oGameData.initGlobalObject = function () {
 
@@ -91,29 +159,38 @@ function validateForm()  {
     const black = '#000000';
     const white = '#ffffff';
 
-
     try {
         //Hämtar alla inputs med typen text och lägger dem i textRefs.
         let textRefs = document.querySelectorAll('input[type=text]');
         //Hämtar alla inputs med typen color och lägger dem i colorRefs.
         let colorRefs = document.querySelectorAll('input[type=color]');
-        //Kontrollerar ifall någon av namn inmatningarna är kortare än 5, om så är fallet så kastas värde till catch.
+
+        //Kontrollerar ifall någon av namn inmatningarna är kortare än 5,
+        //om så är fallet så kastas värde till catch.
         if(textRefs[0].value.length<5 || textRefs[1].value.length<5){
             throw' "nickname" måste vara längre än 5 bokstäver.'
         }
-        //Kontrollerar ifall någon av namn inmatningarna har samma värde, om så är fallet så kastas värde till catch.
+
+        //Kontrollerar ifall någon av namn inmatningarna har samma värde,
+        //om så är fallet så kastas värde till catch.
         if(textRefs[0].value == textRefs[1].value){
             throw ' "nickname" får ej vara likadana.'
         }
-        //Kontrollerar ifall någon färg inmatningarna har samma värde, om så är fallet så kastas värde till catch.
+        
+        //Kontrollerar ifall någon färg inmatningarna har samma värde,
+        //om så är fallet så kastas värde till catch.
         if(colorRefs[0].value == colorRefs[1].value){
             throw ' spelare får inte ha likadan färg.'
         }
-        //Kontrollerar ifall någon färg inmatningarna har färgen svart, om så är fallet så kastas värde till catch.
+
+        //Kontrollerar ifall någon färg inmatningarna har färgen svart,
+        //om så är fallet så kastas värde till catch.
         if(colorRefs[0].value == black || colorRefs[1].value == black){
             throw ' spelarfärg får ej vara svart.'
         }
-        //Kontrollerar ifall någon färg inmatningarna har färgen vit, om så är fallet så kastas värde till catch.
+
+        //Kontrollerar ifall någon färg inmatningarna har färgen vit,
+        //om så är fallet så kastas värde till catch.
         if(colorRefs[0].value == white || colorRefs[1].value == white){
             throw ' spelarfärg får ej vara vit.'
         }
@@ -131,21 +208,28 @@ function validateForm()  {
 function initiateGame(){
     //Definerar let för vilken spelare som ska börja samt dess spelnamn.
     let playerChar, playerName;
+
     //Hämtar form-element ifrån index.html för att klassa i senare skede ska kunna läggas till. Endast ett formulär finns så vi körde på denna metoden.
     let form = document.querySelector('form');
+
     //Lägger till klassen d-none för elementet form.
     form.classList.add("d-none");//Gömmer elementet
     
     //Definerar element med id "game-area" för att kunna ta bort klassen "d-none" i senare skede.
     let gameArea = document.getElementById("game-area");
+
     //Tar bort klassen från game-area
     gameArea.classList.remove("d-none")
+    
     //Ändrar text innehållet för #errorMsg till null.
     document.querySelector('#errorMsg').textContent = "";
+    
     //Hämtar alla inputs med typen text och lägger dem i textRefs.
     let textRefs = document.querySelectorAll('input[type=text]');
+    
     //Hämtar alla inputs med typen color och lägger dem i colorRefs.
     let colorRefs = document.querySelectorAll('input[type=color]');
+    
     //Tilldelar spelar attribut värden från inmatningar som sparats i textRefs och ColorRefs.
     oGameData.nickNamePlayerOne=textRefs[0].value;
     oGameData.nickNamePlayerTwo=textRefs[1].value;
@@ -177,7 +261,11 @@ function initiateGame(){
     //Definerar alla H1:or i klassen jumbotron i index.html.
     let jumbotronH1 = document.querySelector('.jumbotron h1');
     //Tilldelar H1:an namnet som playerName fått.
-    jumbotronH1.textContent = playerName;
+    jumbotronH1.textContent = "Spelare att börja är "+playerName;
+
+    //Lägger till en lyssnare på Tabellen som kallar på executeMove
+    let table = document.querySelector("table");
+    table.addEventListener("click", executeMove);
 
 }
 
@@ -191,7 +279,6 @@ function initiateGame(){
  * Funktionen tar inte emot några värden.
  */
 oGameData.checkForGameOver = function () {
-    
     //Kontrollerar vilka rader som kan resultera i vinster
     //Lägger in raderna i en 2d array
     let kontrolleraVinster = [
@@ -204,7 +291,6 @@ oGameData.checkForGameOver = function () {
         [0, 4, 8],
         [2, 4, 6]
     ];
-
     //Hittat inspiration från geeksforgeeks för att loopa igenom 2d array "https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/"
     //Loopar igenom att Array med möjliga vinster.
     for (let i = 0; i < kontrolleraVinster.length; i++) {
@@ -223,9 +309,6 @@ oGameData.checkForGameOver = function () {
             return 3;
         }
     }
-
-
-    
     // Tidigare IF-sats uteslutet scenariot där ingen vinnare finns och det fortfarande finns plats att spela på. 
     return 0;
 }
